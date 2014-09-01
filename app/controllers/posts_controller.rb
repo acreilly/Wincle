110627af12a4e00bd6717b29fbd387eb4b886429
acreilly <acreilly3@gmail.com>
2014-08-31 20:02:05 -0700

    sdfds

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,12 +2,12 @@ class PostsController < ApplicationController
 include SessionsHelper
 
   def index
-    # employee = current_user.employee
-    # algorithm = SearchAlgorithm.new(employee, Post.all)
-    # ordered_posts = algorithm.search
-    # top_results = ordered_sightings[0..19]
-    # render json: top_results.to_json(methods: [:long_date])
-    @posts = Post.all
+    employee = current_user.employee
+    algorithm = SearchAlgorithm.new(employee, Post.all)
+    ordered_posts = algorithm.search
+    top_results = ordered_posts[0..19]
+    render json: top_results.to_json
+    # @posts = Post.all
   end
 
   def new
