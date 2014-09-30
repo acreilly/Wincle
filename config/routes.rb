aff3b4b34790e463b554f5cc93b79693b240737f
acreilly <acreilly3@gmail.com>
2014-09-30 09:57:09 -0400

    changed routes for employees and employers posts, got rid of employees & employers index

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 Rails.application.routes.draw do
   resources :users
-  resources :employees, only: [:edit, :update, :index]
-  resources :employers, only: [:edit, :update, :index]
+  resources :employees, only: [:edit, :update]
+  resources :employers, only: [:edit, :update]
   resources :posts
   resources :sessions, only: [:create]
   delete "logout", to: "sessions#destroy", as: "logout"
   get "login", to: "users#login", as: "login"
   get "linkedin_callback", to: "users#linkedin_callback", as: "linkedin_callback"
   get "profile", to: "users#profile", as: "profile"
-  get "employees", to: "posts#employees", as: "employees"
-  get "employers", to: "posts#employers", as: "employers"
+  get "posts/employees", to: "posts#employees", as: "employees"
+  get "posts/employers", to: "posts#employers", as: "employers"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
